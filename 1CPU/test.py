6d4e3ca5e44fc2f125b96163d569372af1ec1e51
Alex <asokolsky@yahoo.com>
2019-10-14 19:50:53 -0700

    More verbose output

--- 1CPU/test.py
+++ 1CPU/test.py
@@ -1,4 +1,21 @@
-def Fibonacci(n): 
+#!/usr/bin/env python3
+#
+# launch it like this:
+# $ python3 ./test.py 10
+# or
+# $ python3 ./test.py
+# 
+# Default Fibonacci number to calculate - overwrite it on a command line:
+# $ python3 ./test.py 10
+n = 60
+#
+# nothing to customize below..
+#
+import sys
+import time
+import datetime
+
+def Fibonacci( n ): 
     if n < 0: 
         print("Incorrect input")
         return
@@ -6,12 +23,36 @@ def Fibonacci(n):
     if n == 1: 
         return 0
     # Second Fibonacci number is 1 
-    elif n==2: 
+    if n == 2: 
         return 1
-    return Fibonacci(n-1) + Fibonacci(n-2) 
+    return Fibonacci( n-1 ) + Fibonacci( n-2 ) 
 
-n = 100
-print('Calculating Fibonacci ' + str( n ))
-f = Fibonacci( n )
-print('Fibonacci ' + str( n ) + ' == ' + str( f ))
+def FibonacciVerbose( n ):
+    print( 'Fibonacci(', n, ') => ', end='', flush=True )
+    pt1 = time.process_time()
+    #pc1 = time.perf_counter()    
+    f = Fibonacci( n )
+    #pc2 = time.perf_counter()
+    pt2 = time.process_time()
+    dt = round( pt2 - pt1, 2 )
+    print( f, 'in', datetime.timedelta(seconds=dt) )
+    return
 
+
+if __name__ == '__main__':
+    try:
+        n = int( sys.argv[-1] )
+    except:
+        pass
+
+    try:
+        for i in range(2, n+1):
+            FibonacciVerbose( i )
+    except KeyboardInterrupt:
+        #print( '\nProcess KeyboardInterrupt' )
+        pass
+    except Exception as e:
+        print( '\nProcess caught:', str( e ) )
+    finally:
+        print( '' )
+        pass
--- CPUs/test.py
+++ CPUs/test.py
@@ -1,19 +1,24 @@
+#!/usr/bin/env python3
 #
-#
-#
-
-# # of CPUs to use in parallel
-cpus = 3
-# Fibbonacci number to calculate
+# launch it like this:
+# $ python3 ./test.py 10 2
+# or
+# $ python3 ./test.py
+# 
+# Default Fibonacci number to calculate - overwrite it on a command line:
+# $ python3 ./test.py 10 2
 n = 50
-
+# number of CPUs to use in parallel - overwrite it on a command line:
+# $ python3 ./test.py 10 2
+cpus = 3
+#
 # nothing to customize below..
+#
+import sys
+import os
+import time
+import datetime
 from multiprocessing import Pool
-
-cachedResult = []
-for i in range(n + 1):
-    cachedResult.append( 0 )
-#print(len(cachedResult))
 
 def Fibonacci( n ): 
     if n < 0: 
@@ -25,38 +30,54 @@ def Fibonacci( n ):
     # Second Fibonacci number is 1 
     if n == 2: 
         return 1
+    return Fibonacci(n-1) + Fibonacci(n-2)
 
-    global cachedResult
-    res = cachedResult[ n ]
-    if( res ):
-        return res
-    res = Fibonacci(n-1) + Fibonacci(n-2)
-    #cachedResult[ n ] = res
-    #print('Fibonacci', str( n ), '=>', str( res ))
-    return res
+def FibonacciVerbose( n ):
+    pt1 = time.process_time()
+    #pc1 = time.perf_counter()    
+    f = Fibonacci( n )
+    #pc2 = time.perf_counter()
+    pt2 = time.process_time()
+    dt = round( pt2 - pt1, 2 )
+    print( os.getpid(), 'Fibonacci(', n, ') => ', f, 'in', datetime.timedelta(seconds=dt) )
+    return f
 
 def FibonacciWrap( n ):
     try:
-        return Fibonacci( n )
+        for i in range(2, n+1):
+            FibonacciVerbose( i )
+        return 0
     except KeyboardInterrupt:
-        print( 'Process KeyboardInterrupt' )
+        #print( 'Worker KeyboardInterrupt' )
+        pass
     except Exception as e:
-        print( 'Process caught:', str( e ) )
+        print( 'Worker caught:', str( e ) )
     finally:
-        print( 'Process cleanup' )
+        #print( 'Worker cleanup' )
+        pass
     return 0
 
 if __name__ == '__main__':
-    print('Calculating Fibonacci', str( n ), 'using', str(cpus), ' CPUs')
+    try:
+        n = int( sys.argv[-2] )
+        cpus = int( sys.argv[-1] )
+    except:
+        pass
+
+    print( 'Calculating Fibonacci', n, 'using', cpus, 'CPUs' )
+
     p = Pool( cpus )
     try:
         f = p.map( FibonacciWrap, [n] * cpus )
         p.close()
         p.join()
-        print( 'Fibonacci', str( n ), '==', str( f ), ' * ', str( cpus ) )
+        print( 'Fibonacci', n, '=>', f )
+
     except KeyboardInterrupt:
-        print( 'Main KeyboardInterrupt' )
+        print( '' )
+        pass
     except Exception as e:
         print( 'Main caught:', str( e ) )
     finally:
-        print( 'Main cleanup' )
+        #print( 'Main cleanup' )
+        pass
